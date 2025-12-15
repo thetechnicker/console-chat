528c9b82d8647054bee369390a2be2160536d60a
Thetechnicker <106349149+thetechnicker@users.noreply.github.com>
2025-12-15 15:10:19 +0100

    cargo fix

--- console-chat/src/network/mod.rs
+++ console-chat/src/network/mod.rs
@@ -1,9 +1,5 @@
-use lazy_static::lazy_static;
-use openapi::apis::{configuration, rooms_api, users_api};
-use serde::Serialize;
+use openapi::apis::configuration;
 use std::sync::Arc;
 use std::sync::OnceLock;
-use tokio::sync::Mutex;
-use tokio::sync::OnceCell;
 
 pub static CLIENT: OnceLock<Arc<configuration::Configuration>> = OnceLock::new();
