4eaabe781fffd2a0c8a4a1556322533700f00553
Thetechnicker <106349149+thetechnicker@users.noreply.github.com>
2025-12-21 16:31:06 +0100

    empty docstrings again

--- console-chat/openapi-custom/src/models/message_type.rs
+++ console-chat/openapi-custom/src/models/message_type.rs
@@ -11,7 +11,6 @@
 use crate::models;
 use serde::{Deserialize, Serialize};
 
-///
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 #[serde(tag = "type")]
 pub enum MessageType {
